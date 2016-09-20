e3a066450278f755947e2a8819931cb95bb03de2
Christian Jensen <cbejensen@gmail.com>
2016-09-20 14:57:17 -0600

    created navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router'
 import NavLink from '../NavLink/NavLink'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
 
 export function Header(props) {
   return (
-    <div>
-      <Link to="/">
-        <img src="" alt="WISHFULL"/>
-      </Link>
-      <nav role="navigation">
-        <NavLink to="/home">Home</NavLink>
-        {/* <NavLink to="/find-a-friend">Find A Friend</NavLink>
-        <NavLink to="/about">About</NavLink> */}
-      </nav>
-    </div>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">
+            <img src="" alt="WISHFULL"/>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem>
+            <NavLink to="/home">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/search">Search</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem>
+            <NavLink to="/sign-in">Sign In</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
