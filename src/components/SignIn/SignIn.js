f4074c37670dbc4919b7c83a56625fb4c0ec8f35
Christian Jensen <cbejensen@gmail.com>
2016-09-25 16:59:18 -0600

    sign in works

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,7 +4,7 @@ import * as firebase from 'firebase';
 const SignInContainer = React.createClass({
   getInitialState() {
     return {
-      username: '',
+      email: '',
       password: ''
     }
   },
@@ -17,17 +17,45 @@ const SignInContainer = React.createClass({
       }
     });
   },
-
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  },
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.email, this.state.password);
+    firebase.auth().signInWithEmailAndPassword(
+      this.state.email, this.state.password).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode, errorMessage)
+    });
+  },
   render() {
-    return <SignIn />
+    return <SignIn
+      email={this.state.email}
+      password={this.state.password}
+      changeEmail={this.handleEmailChange}
+      changePassword={this.handlePasswordChange}
+      submit={this.handleSubmit}/>
   }
 });
 
 export function SignIn(props) {
+  console.log(props);
   return (
-    <form action="">
-      Username: <input type="text"/>
-      Password: <input type="text"/>
+    <form onSubmit={props.submit}>
+      Email: <input
+        type="text"
+        value={props.email}
+        onChange={props.changeEmail}/>
+      Password: <input
+        type="text"
+        value={props.password}
+        onChange={props.changePassword}/>
+      <button type="submit">Submit</button>
     </form>
   );
 }
