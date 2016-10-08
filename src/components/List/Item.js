aa8cf93fd86807ddc709e4fa9e66c9ebae495777
Christian Jensen <cbejensen@gmail.com>
2016-10-08 15:31:41 -0600

    added ability to add new wishes

--- src/components/List/Item.js
+++ src/components/List/Item.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import { Row, Col } from 'react-bootstrap';
 
-export default function Item(props) {
+export default function Item({ item }) {
   return (
     <div><Row>
-      <Col xs={4}><a href={props.item.url}>{props.item.title}</a></Col>
-      <Col xs={4}>{props.item.price}</Col>
-      <Col xs={4}>{props.item.priority}</Col>
-      <Col xs={12}>{props.item.description}</Col>
+      <Col xs={4}><a href={item.url}>{item.title}</a></Col>
+      <Col xs={4}>
+        {item.price ? item.price : null}
+      </Col>
+      <Col xs={4}>
+        {item.priority ? item.priority : null}
+      </Col>
+      <Col xs={12}>
+        {item.description ? item.description : null}
+      </Col>
 
     </Row><hr/></div>
   );
--- src/routes/NewWish.js
+++ src/routes/NewWish.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import FormInput from '../components/FormInput'
-import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import FormInput from '../components/FormInput';
+import { FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
+import { addWish } from '../utils/firebaseHelpers';
+import { browserHistory } from 'react-router';
 
 const NewWishContainer = React.createClass({
   getInitialState() {
@@ -8,9 +10,8 @@ const NewWishContainer = React.createClass({
       title: '',
       description: '',
       link: '',
-      price: '',
-      priority: '',
-      notes: ''
+      price: 0,
+      priority: 1,
     }
   },
   handleChange(field, e) {
@@ -18,25 +19,35 @@ const NewWishContainer = React.createClass({
     newState[field] = e.target.value;
     this.setState(newState);
   },
+  handleSubmit(e) {
+    e.preventDefault();
+    addWish(this.state, this.props.params.user).then(res => {
+      console.log(res)
+      browserHistory.push(`/${this.props.params.user}`)
+    }, err => {
+      alert(err)
+    })
+  },
   render() {
     return (
     <form onSubmit={this.handleSubmit}>
       <FormInput label='Title'
         value={this.state.title}
         onChange={this.handleChange.bind(this, 'title')}
-        />
+        required />
       <FormInput label='Description'
         value={this.state.description}
         onChange={this.handleChange.bind(this, 'description')}
-        help='Describe what the product is and why you want it'/>
+        help='Describe what the product is and why you want it' />
       <FormInput label='Link'
         value={this.state.link}
         onChange={this.handleChange.bind(this, 'link')}
-        />
+        placeholder='http://example.com'
+        help='Copy and paste a URL link to this item on the web' />
       <FormInput label='Price'
         value={this.state.price}
         onChange={this.handleChange.bind(this, 'price')}
-        />
+        help='Do not include a dollar sign' />
       <FormGroup>
         <ControlLabel>Priority</ControlLabel>
         <FormControl componentClass="select" placeholder="select">
@@ -51,6 +62,10 @@ const NewWishContainer = React.createClass({
           <option value={9}>9</option>
           <option value={10}>10</option>
         </FormControl>
+        <HelpBlock>
+          1 = I barely even want this <br />
+          10 = I practically need this to survive
+        </HelpBlock>
       </FormGroup>
       <Button type="submit">Wish!</Button>
     </form>
