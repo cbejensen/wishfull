12c15a6ed7f6e9ae4e1f5b4f5dc65a089901e20b
Christian Jensen <cbejensen@gmail.com>
2016-12-18 04:38:03 -0700

    cleaned up avatar form and user heading

---
+++ src/components/User/AvatarForm/AvatarFormContainer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import AvatarForm from './AvatarForm';
+import { getFile, uploadFile } from 'utils/firebaseHelpers';
+
+class AvatarFormContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newAvatar: null,
+      loading: ''
+    }
+    this.selectAvatar = this.selectAvatar.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  selectAvatar(e) {
+    var img = e.target.files[0]
+    this.setState({
+      newAvatar: img
+    })
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.newAvatar) {
+      this.setState({loading: 'Loading...'})
+      const file = this.state.newAvatar;
+      const path = `images/avatars/${this.props.user.uid}`;
+      uploadFile(file, path).then(res => {
+        alert('Success!');
+        getFile(`images/avatars/${this.props.user.uid}`).then(avatar => {
+          this.setState({
+            loading: ''
+          })
+        })
+      }, err => {
+        alert('There was an error. Please try again.')
+        console.log(err)
+      })
+    } else {
+      alert('Please choose a picture first')
+    }
+  }
+  render() {
+    return <AvatarForm loading={this.state.loading}
+      selectAvatar={this.selectAvatar}
+      handleSubmit={this.handleSubmit}/>
+  }
+};
+
+AvatarFormContainer.propTypes = {
+  user: React.PropTypes.object.isRequired
+}
+
+export default AvatarFormContainer
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserHeading from 'components/User/UserHeading';
+import { AvatarForm } from 'components/User/AvatarForm';
 import { CheckAuth } from '../components/CheckAuth';
 import { UserItem } from '../components/User';
 import { browserHistory } from 'react-router';
-import { Grid, Row, Col, Nav, NavItem, Button } from 'react-bootstrap';
+import { Grid, Nav, NavItem } from 'react-bootstrap';
 import { WishList } from '../components/WishList/WishList';
 import { getFile, uploadFile } from '../utils/firebaseHelpers';
 import * as firebase from 'firebase';
@@ -11,52 +13,21 @@ class HomeContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTab: 1,
-      loading: false
+      activeTab: 1
     };
     this.handleTabSelect = this.handleTabSelect.bind(this);
-    this.handleAvatarSelect = this.handleAvatarSelect.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleTabSelect(e) {
     this.setState({
       activeTab: e
     })
   }
-  handleAvatarSelect(e) {
-    var img = e.target.files[0]
-    this.setState({
-      pendingAvatar: img
-    })
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.pendingAvatar) {
-      this.setState({loading: 'Loading...'})
-      const file = this.state.pendingAvatar;
-      const path = `images/avatars/${this.props.user.uid}`;
-      uploadFile(file, path).then(res => {
-        alert('Success!');
-        getFile(`images/avatars/${this.props.user.uid}`).then(avatar => {
-          this.setState({
-            avatar: avatar,
-            loading: false
-          })
-        })
-      }, err => {
-        alert('There was an error. Please try again.')
-        console.log(err)
-      })
-    } else {
-      alert('Please choose a picture first')
-    }
-  }
   render() {
     return (
       <CheckAuth>
         <Home user={this.props.user}
           activeTab={this.state.activeTab}
-          handleTabSelect={e => {this.handleTabSelect.bind(null, e)}}/>
+          handleTabSelect={this.handleTabSelect}/>
       </CheckAuth>
     )
   }
@@ -67,7 +38,6 @@ const Home = props => {
     textAlign: 'center',
     paddingTop: '10px'
   }
-  if (!props.user) return null;
   let activeComponent;
   if (props.activeTab === 1) {
     activeComponent = (
@@ -75,18 +45,12 @@ const Home = props => {
     )
   } else {
     activeComponent = (
-      <form onSubmit={props.handleSubmit} style={style}>
-        <input type="file" onChange={props.handleAvatarSelect} />
-        <Button bsStyle="primary" type="submit"
-          style={{margin: '20px'}}>Submit</Button>
-        {props.loading && props.loading}
-        <UserItem id={props.user.uid}/>
-        Refresh page after submitting new avatar.
-      </form>
+      <AvatarForm user={props.user} />
     )
   }
   return (
     <Grid>
+      <UserHeading user={props.user} />
       <Nav bsStyle="tabs" activeKey={props.activeTab}
         onSelect={props.handleTabSelect} justified >
         <NavItem eventKey={1}>My Wish List</NavItem>
