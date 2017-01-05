72095cee0a8b554052007dca934d15bd82435d00
Christian Jensen <cbejensen@gmail.com>
2017-01-05 02:34:44 -0700

    matched user heading to item box design

--- src/components/User/UserHeading.js
+++ src/components/User/UserHeading.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Avatar from './Avatar';
+import ItemBox from 'components/ItemBox';
 import { getUser, getFile } from '../../utils/firebaseHelpers';
 import { Row, Col } from 'react-bootstrap';
 import './UserHeading.css';
 
 const UserHeading = props => {
   return (
-    <Row className="UserHeading-row"
-      onClick={props.handleClickUser}>
-      <Col xs={4} sm={3} className="UserHeading-img-col">
-        <Avatar uid={props.uid} />
-      </Col>
-      <Col xs={8} sm={9} className="UserHeading-name-col">
-        <div>{props.name}</div>
+    <Row>
+      <Col xs={12}>
+        <ItemBox handleClick={props.handleClickUser}>
+          <Row className="UserHeading-row">
+            <Col xs={4} sm={3} className="UserHeading-img-col">
+              <Avatar uid={props.uid} />
+            </Col>
+            <Col xs={8} sm={9} className="UserHeading-name-col">
+              <div>{props.name}</div>
+            </Col>
+          </Row>
+        </ItemBox>
       </Col>
     </Row>
   )
