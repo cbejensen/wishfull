3706a5d9dabe7c38539fd3e134954f301fa5958a
Christian Jensen <cbejensen@gmail.com>
2017-01-13 16:34:38 -0700

    added subittle to user heading

---
+++ src/components/User/UserHeadingSubtitle.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {getList} from 'utils/firebaseHelpers';
+
+class UserHeadingSubtitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {wishCount: null}
+  }
+  componentDidMount() {
+    getList(this.props.uid).then(list => {
+      console.log(list)
+      const wishCount = Object.keys(list).length;
+      console.log(wishCount)
+      this.setState({wishCount: wishCount})
+    }, err => {
+      console.log(err);
+    })
+  }
+  render() {
+    if (!this.state.wishCount) return null;
+    return (
+      <div className="text-muted" style={{fontSize: '.5em'}}>
+        {`${this.state.wishCount} wishes`}
+      </div>
+    )
+  }
+};
+
+UserHeadingSubtitle.propTypes = {
+  uid: React.PropTypes.string.isRequired
+}
+
+export default UserHeadingSubtitle;
