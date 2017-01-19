acad58ba1853b2df293b0617caea2d4f842f4992
Christian Jensen <cbejensen@gmail.com>
2017-01-19 02:15:35 -0700

    improved registration ui for async submit button

--- src/components/Login/CreateAccount.js
+++ src/components/Login/CreateAccount.js
@@ -8,11 +8,14 @@ class CreateAccountContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      avatar: null
+      user: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        avatar: null
+      },
+      creatingUser: false
     }
     this.validatePassword = this.validatePassword.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -20,7 +23,8 @@ class CreateAccountContainer extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   validatePassword() {
-    const length = this.state.password.length
+    console.log(this.state);
+    const length = this.state.user.password.length
     if (length === 0)
       return null
     else if (length < 6)
@@ -29,19 +33,21 @@ class CreateAccountContainer extends React.Component {
       return 'success'
     }
   handleChange(field, e) {
-    const newState = {}
-    newState[field] = e.target.value
-    this.setState(newState)
+    console.log(this.state);
+    // const user = {}
+    // user[field] = e.target.value
+    this.setState({user: {...this.state.user, [field]: e.target.value}})
   }
   handleAvatarChange(e) {
     var img = e.target.files[0]
     console.log(img)
-    this.setState({avatar: img})
+    this.setState({user: {...this.state.user, avatar: img}})
   }
   handleSubmit(e) {
     e.preventDefault()
-    if (this.state.password.length >= 6) {
-      createUser(this.state).then(res => {
+    if (this.state.user.password.length >= 6) {
+      this.setState({creatingUser: true})
+      createUser(this.state.user).then(res => {
         browserHistory.push('/home')
       }, err => {
         alert(err)
@@ -58,13 +64,13 @@ class CreateAccountContainer extends React.Component {
 export function CreateAccount(props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <FormInput label='First Name' required value={props.firstName} onChange={props.handleChange.bind(null, 'firstName')}/>
-      <FormInput label='Last Name' required value={props.lastName} onChange={props.handleChange.bind(null, 'lastName')}/>
-      <FormInput label='E-mail' required type='email' value={props.email} onChange={props.handleChange.bind(null, 'email')}/>
-      <FormInput label='Password' required type='password' help='Must be at least 6 characters' pattern='.{6,}' title='Choose a password that is 6 characters or longer' getValidation={props.validatePassword()} value={props.password} onChange={props.handleChange.bind(null, 'password')}/>
+      <FormInput label='First Name' required value={props.user.firstName} onChange={props.handleChange.bind(null, 'firstName')}/>
+      <FormInput label='Last Name' required value={props.user.lastName} onChange={props.handleChange.bind(null, 'lastName')}/>
+      <FormInput label='E-mail' required type='email' value={props.user.email} onChange={props.handleChange.bind(null, 'email')}/>
+      <FormInput label='Password' required type='password' help='Must be at least 6 characters' pattern='.{6,}' title='Choose a password that is 6 characters or longer' getValidation={props.validatePassword()} value={props.user.password} onChange={props.handleChange.bind(null, 'password')}/>
       <FormInput label='Profile Picture' type='file' onChange={props.handleAvatarChange}/>
-      <Button type="submit" bsStyle="primary" bsSize="large">
-        Let's Do This!
+      <Button type={props.creatingUser ? 'button' : 'submit'} bsStyle="primary" bsSize="large" disabled={props.creatingUser}>
+        {props.creatingUser ? 'Registering...' : 'Let\'s Do This!'}
       </Button>
     </form>
   )
