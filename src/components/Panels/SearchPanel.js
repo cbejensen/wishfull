62779bf5c0c6c3a4c9510f1a6e799a03e0c8b24f
Christian Jensen <cbejensen@gmail.com>
2017-02-28 00:52:20 -0700

    redesigned panels

--- src/components/Panels/SearchPanel.js
+++ src/components/Panels/SearchPanel.js
@@ -7,35 +7,41 @@ export default function SearchPanel(props) {
   const styles = {
     container: {
       background: 'linear-gradient(#383838, #000000)',
-      minHeight: '80vh'
+      minHeight: '100vh',
+      paddingTop: '15px'
     },
     header: {
       color: '#ffffff',
       textAlign: 'center',
       fontSize: '6vmin'
     },
+    search: {
+      marginBottom: '5vmin'
+    },
     textLink: {
+      position: 'absolute',
+      bottom: '0',
+      width: '100%',
       textAlign: 'center',
-      padding: '15px 0'
+      paddingBottom: '5px'
     }
   }
   return (
     <div style={styles.container}>
-      <Grid>
-        <h3 style={styles.header}>See who's on Wishfull</h3>
+      <h3 style={styles.header}>See who's on Wishfull</h3>
+      <div style={styles.search}>
         <Search
           excludeWishes
-          categories={['users']}
           placeHolder="Search a friend's name"
           userNameColor="#dadada" />
-        <div style={styles.textLink}>
-          <TextLink
-            text='Join Now'
-            link='/sign-up'
-            color='#ffffff'
-            fontSize='4vmin' />
-        </div>
-      </Grid>
+      </div>
+      <div style={styles.textLink}>
+        <TextLink
+          text='Join Now'
+          link='/sign-up'
+          color='#ffffff'
+          fontSize='4vmin' />
+      </div>
     </div>
   )
 };
