9a9f7d148e2f2ef3c9235d0e641aa9a4d55f3c45
Christian Jensen <cbejensen@gmail.com>
2017-10-02 21:45:05 -0600

    fixed up styling in wish form

--- src/components/Wish/WishForm/WishForm.js
+++ src/components/Wish/WishForm/WishForm.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import FormInput from 'components/FormInput'
-import {FormGroup,
+import React from 'react';
+import FormInput from 'components/FormInput';
+import {
+  FormGroup,
   ControlLabel,
   FormControl,
   HelpBlock,
   Row,
   Col,
-  Button} from 'react-bootstrap'
-import {Link} from 'react-router'
+  Button
+} from 'react-bootstrap';
+import { Link, browserHistory } from 'react-router';
 
 export default function WishForm(props) {
   const styles = {
@@ -17,39 +19,74 @@ export default function WishForm(props) {
     },
     btnRow: {
       textAlign: 'center',
-      width: '100%'
+      margin: '30px auto'
+    },
+    btn: {
+      width: '100%',
+      marginBottom: '20px'
     }
-  }
+  };
+  const cancel = () => {
+    const confirmation = confirm('Are you sure you want to cancel?');
+    if (confirmation) {
+      browserHistory.push('home');
+    }
+  };
+  const deleteWish = () => {
+    const confirmation = confirm(
+      'Are you sure you want to delete this wish? This action cannot be undone.'
+    );
+    if (confirmation) {
+      props.handleDelete();
+    }
+  };
+  const submitBtn = (
+    <Col xs={props.wishId ? 12 : 6}>
+      <Button bsStyle="primary" type="submit" style={styles.btn}>
+        {props.wishId ? 'Save' : 'Submit'}
+      </Button>
+    </Col>
+  );
   return (
     <form style={styles.form} onSubmit={props.handleSubmit}>
-      <FormInput label='Title'
+      <FormInput
+        label="Title"
         autoFocus
         ref={e => focus(e)}
         value={props.title}
         onChange={props.handleChange.bind(null, 'title')}
-        required />
-      <FormInput label='Description'
+        required
+      />
+      <FormInput
+        label="Description"
         value={props.description}
         onChange={props.handleChange.bind(null, 'description')}
         componentClass="textarea"
-        help='Describe what the product is and why you want it' />
-      <FormInput label='URL'
+        help="Describe what the product is and why you want it"
+      />
+      <FormInput
+        label="URL"
         value={props.url}
         onChange={props.handleChange.bind(null, 'url')}
         getValidation={props.validateUrl()}
-        placeholder='example.com'
-        help='Copy and paste a link to this item on the web' />
-      <FormInput label='Price'
+        placeholder="example.com"
+        help="Copy and paste a link to this item on the web"
+      />
+      <FormInput
+        label="Price"
         value={props.price}
         onChange={props.handleChange.bind(null, 'price')}
         getValidation={props.validatePrice()}
-        help='Only whole numbers' />
+        help="Only whole numbers"
+      />
       <FormGroup>
         <ControlLabel>Priority</ControlLabel>
-        <FormControl value={props.priority}
+        <FormControl
+          value={props.priority}
           onChange={props.handleChange.bind(null, 'priority')}
           componentClass="select"
-          placeholder="select">
+          placeholder="select"
+        >
           <option>1</option>
           <option>2</option>
           <option>3</option>
@@ -62,27 +99,26 @@ export default function WishForm(props) {
           <option>10</option>
         </FormControl>
         <HelpBlock>
-          1 = I barely even want this <br />
-          10 = I practically need this to survive
+          1 = I barely want this <br />
+          10 = I really want this
         </HelpBlock>
       </FormGroup>
       <Row style={styles.btnRow}>
-        <Col xs={props.wishId ? 4 : 6} style={{marginBottom: '30px'}}>
-          <Button bsStyle="primary" type="submit">
-            {props.wishId ? 'Save' : 'Submit'}
+        {props.wishId && submitBtn}
+        {props.wishId && (
+          <Col xs={6}>
+            <Button bsStyle="danger" onClick={deleteWish} style={styles.btn}>
+              Delete
+            </Button>
+          </Col>
+        )}
+        <Col xs={6}>
+          <Button bsStyle="warning" onClick={cancel} style={styles.btn}>
+            Cancel
           </Button>
         </Col>
-        <Col xs={props.wishId ? 4 : 6} style={{marginBottom: '30px'}}>
-          <Link to='/home'>
-            <Button bsStyle="warning">Cancel</Button>
-          </Link>
-        </Col>
-        {props.wishId && <Col xs={4} style={{marginBottom: '30px'}}>
-          <Button bsStyle="danger" onClick={props.handleDelete}>
-            Delete
-          </Button>
-        </Col>}
+        {!props.wishId && submitBtn}
       </Row>
     </form>
-  )
+  );
 }
--- src/components/Wish/WishForm/WishFormContainer.js
+++ src/components/Wish/WishForm/WishFormContainer.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import WishForm from './WishForm'
-import {browserHistory} from 'react-router'
-import {addWish,
+import React from 'react';
+import WishForm from './WishForm';
+import { browserHistory } from 'react-router';
+import {
+  addWish,
   updateWish,
   getWish,
-  deleteWish} from 'utils/firebaseHelpers'
-import {validateWish,
+  deleteWish
+} from 'utils/firebaseHelpers';
+import {
+  validateWish,
   validatePrice as validatePriceHelper,
-  validateUrl as validateUrlHelper} from 'utils/validationHelpers'
+  validateUrl as validateUrlHelper
+} from 'utils/validationHelpers';
 
 class WishFormContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     // TODO: add security rules to encapsulate fulfilled
     this.state = {
       title: '',
@@ -19,86 +23,100 @@ class WishFormContainer extends React.Component {
       url: '',
       price: '',
       priority: 5
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.validateUrl = this.validateUrl.bind(this)
-    this.validatePrice = this.validatePrice.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateUrl = this.validateUrl.bind(this);
+    this.validatePrice = this.validatePrice.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     if (this.props.wishId) {
-      getWish(this.props.uid, this.props.wishId).then(wish => {
-        this.setState({
-          title: wish.title,
-          description: wish.description,
-          url: wish.url,
-          price: wish.price.toString(),
-          priority: wish.priority.toString()
-        })
-      }, err => {
-        browserHistory.push('/home')
-      })
+      getWish(this.props.uid, this.props.wishId).then(
+        wish => {
+          this.setState({
+            title: wish.title,
+            description: wish.description,
+            url: wish.url,
+            price: wish.price.toString(),
+            priority: wish.priority.toString()
+          });
+        },
+        err => {
+          browserHistory.push('/home');
+        }
+      );
     }
   }
   handleChange(field, e) {
-    const newState = {}
-    newState[field] = e.target.value
-    this.setState(newState)
+    const newState = {};
+    newState[field] = e.target.value;
+    this.setState(newState);
   }
   validateUrl() {
-    let url = this.state.url
-    if (url === '') return null
-    let verified = validateUrlHelper(url)
-    return verified ? 'success' : 'error'
+    let url = this.state.url;
+    if (url === '') return null;
+    let verified = validateUrlHelper(url);
+    return verified ? 'success' : 'error';
   }
   validatePrice() {
-    let price = this.state.price
+    let price = this.state.price;
     if (price === '') {
-      return null
+      return null;
     } else {
-      let verified = validatePriceHelper(price)
-      return verified ? 'success' : 'error'
+      let verified = validatePriceHelper(price);
+      return verified ? 'success' : 'error';
     }
   }
   handleDelete(e) {
-    const confirmed = confirm('WARNING\nThis wish will be permanently deleted.')
-    if (confirmed) deleteWish(this.props.uid, this.props.wishId)
-    browserHistory.push('/home')
+    deleteWish(this.props.uid, this.props.wishId);
+    browserHistory.push('/home');
   }
   handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     let wish = {
       title: this.state.title,
       description: this.state.description,
       url: this.state.url,
       price: this.state.price,
       priority: this.state.priority
-    }
-    if (wish.price) wish.price = parseInt(wish.price.replace(/,/g, ''), 10)
-    wish.priority = parseInt(wish.priority, 10)
-    validateWish(wish).then(res => {
-      const onSuccess = res => browserHistory.push('/home')
-      const onError = err => alert('There was an error processing your request. Please try again.')
-      if (!this.props.wishId) {
-        addWish(wish, this.props.uid).then(onSuccess, onError)
-      } else {
-        updateWish(this.props.uid, this.props.wishId, wish).then(onSuccess, onError)
+    };
+    if (wish.price) wish.price = parseInt(wish.price.replace(/,/g, ''), 10);
+    wish.priority = parseInt(wish.priority, 10);
+    validateWish(wish).then(
+      res => {
+        const onSuccess = res => browserHistory.push('/home');
+        const onError = err =>
+          alert(
+            'There was an error processing your request. Please try again.'
+          );
+        if (!this.props.wishId) {
+          addWish(wish, this.props.uid).then(onSuccess, onError);
+        } else {
+          updateWish(this.props.uid, this.props.wishId, wish).then(
+            onSuccess,
+            onError
+          );
+        }
+      },
+      err => {
+        alert(err);
       }
-    }, err => {
-      alert(err)
-    })
+    );
   }
   render() {
-    return <WishForm
-      {...this.state}
-      wishId={this.props.wishId}
-      handleChange={this.handleChange}
-      validateUrl={this.validateUrl}
-      validatePrice={this.validatePrice}
-      handleDelete={this.handleDelete}
-      handleSubmit={this.handleSubmit} />
+    return (
+      <WishForm
+        {...this.state}
+        wishId={this.props.wishId}
+        handleChange={this.handleChange}
+        validateUrl={this.validateUrl}
+        validatePrice={this.validatePrice}
+        handleDelete={this.handleDelete}
+        handleSubmit={this.handleSubmit}
+      />
+    );
   }
 }
 
-export default WishFormContainer
+export default WishFormContainer;
