8c83fca672d5d19d1df545f727376b434bd00d45
Christian Jensen <cbejensen@gmail.com>
2017-11-28 03:46:08 -0700

    not sure why this works yet but it does

--- src/components/User/FriendList.js
+++ src/components/User/FriendList.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import {Search} from 'components/Search'
-import {getFriends} from '../../utils/firebaseHelpers'
-import UserList from './UserList'
+import React from 'react';
+import { Search } from 'components/Search';
+import { getFriends } from '../../utils/firebaseHelpers';
+import UserList from './UserList';
 
 class FriendList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       friends: null,
       showUserList: true
-    }
-    this.handleSearch = this.handleSearch.bind(this)
+    };
+    this.handleSearch = this.handleSearch.bind(this);
   }
   componentDidMount() {
     getFriends(this.props.uid).then(friends => {
-      this.setState({friends: friends})
-    })
+      this.setState({ friends: friends });
+    });
   }
   handleSearch(query) {
-    this.setState({showUserList: query === ''})
+    this.setState({ showUserList: query === '' });
   }
   render() {
     const styles = {
@@ -27,28 +27,33 @@ class FriendList extends React.Component {
         paddingTop: '30px',
         textAlign: 'center'
       }
-    }
+    };
     if (!this.props.uid) {
-      return null
+      return null;
     } else if (!this.state.friends) {
-      return <div style={styles.textBox}>Loading...</div>
+      return <div style={styles.textBox}>Loading...</div>;
     } else if (this.state.friends.length < 1) {
-      return <div style={styles.textBox}>You have no friends!</div>
+      return <div style={styles.textBox}>You have no friends!</div>;
     } else {
       return (
         <div>
           <Search
             uid={this.props.uid}
-            placeHolder='Search by name'
+            placeHolder="Search by name"
             handleQueryChange={this.handleSearch}
             focusInput={this.props.focusInput}
             excludeUsersNotFriends
-            excludeWishes />
-          {this.state.showUserList &&
-            <UserList users={this.state.friends} uid={this.props.uid} />
-          }
+            excludeWishes
+          />
+          {this.state.showUserList && (
+            <UserList
+              users={this.state.friends}
+              uid={this.props.uid}
+              luminosity="dark"
+            />
+          )}
         </div>
-      )
+      );
     }
   }
 }
@@ -56,6 +61,6 @@ class FriendList extends React.Component {
 FriendList.propTypes = {
   uid: React.PropTypes.node,
   focusInput: React.PropTypes.bool
-}
+};
 
-export default FriendList
+export default FriendList;
